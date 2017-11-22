92134d9f7741ffb9683941a27eedc0a1226d6072
c-guozhi90 <c.guozhi90@gmail.com>
2017-11-22 14:27:27 +0800

    searching function
    adding searching function to the website

--- searching.php
+++ searching.php
@@ -1,39 +1,80 @@
 <?php
 /*
-link to the database and retrieve data, which will be stored in the $_post constant along with 
+link to the database and retrieve data, which will be stored in the $_SESSION constant along with
 the redirection back to the previous page.
+we can set the result as a buffer in the server with the static data type
+then when we want to read information in other pages we can retrieve data from
+this buffer quickly
 */
+
+session_start();
+if(!isset($_GET['keywordsInput'])||!isset($_GET['keywordsType'])){
+  //redirect the page if the input from users are invalid
+}
+
+if(!isset($_SESSION['keywordsInput'])){
+  $_SESSION['keywordsInput']=$_GET['keywordsInput'];
+}
+else if($_SESSION['keywordsInput']==$_GET['keywordsInput']&&isset($_SESSION['resultBuffer'])){
+  $_SESSION['result']=processHtml($_SESSION['resultBuffer'],$_GET['page']);
+  //redirection
+}
+else{
+
+}
 $keywordsString=processString($_GET['keywordsInput']);
-$resultSet=makeConnection($keywordsString);
-$_POST['result']=returnResult($resultset);
+
+$_SESSION['resultBuffer']=makeConnection($keywordsString);
+$_SESSION['resultSet']=processHtml($resultSet,$_GET['page']);
+
+
 function processString($keywords){
     //this function is used to split input string into separate words by regular expression
     //assume that the key words are separated by spaces(one or more)
     $pattern="/\s+/";
     $separatedWords=preg_split($pattern, $keywords);
+
     return $separatedwords;
 }
 function makeConnection($keywords){
     //make a connection and a query to database and return the result set
     $host='localhost';$user;$password;
-    $conn=mysqli_connect($host,$user,$password);
-    if(!$conn)
+    $conn=new mysqli($host,$user,$password);
+    if(mysqli_connect_errno())
         return null;
     //
     else{
-    $sql="select * from kol where ";
+    $sql="select * from kol where ".$_GET['keywordsType'];
     foreach ($keywords as $word)
         $sql=$sql."like '$word' and";
-    $sql=$sql." 1=1 limit 100";
-    mysqli_query($conn, $sql);
-    $result=mysqli_execute($sql);
-    mysqli_close($conn);
+    $sql=$sql." 1=1";
+    $conn->query($sql);
+    $result=$conn->store_result();
+    $conn->close();
     return $result;
     }
 }
-function returnResult($resultWithoutHtml){
-    //return the result to the previous page with added html tags 
-    $htmlText;
-    return $htmlText;
+function processHtml(mysqli_result $resultSet,int $page){
+  $html="";$temp="";
+  //set the default page of the result set
+  if(!isset($page))$page=1;
+  elseif ($page>$resultSet->num_rows/20+1||$page<1) {
+    $page=1;
+  }
+  $resultSet->data_seek(($page-1)*20);
+  for($count=0;$count<20;$count++){
+    $temp=$resultSet->fetch_assoc();
+    $name=$temp['name'];
+    $picturePath=$temp['picturePath'];
+    $kolId=$temp['kolId'];
+    $description=$temp['description'];
+    $html=$html."<div>
+      <div style='float:left'>
+        <img src='$picturePath' alt='$name's picture'/>
+        <a href='$kolId'></a>
+      </div>
+      <div>$description</div>
+    </div>";
+  }
 }
 ?>
--- searchingResult.php
+++ searchingResult.php
@@ -1,25 +1,36 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-
+  <meta charset="utf-8">
+  <script type="text/javascript" src="js\jquery-3.2.1.slim.min.js"></script>
 </head>
 <body>
-<form action="newfile.php">
+  <<?php session_start() ?>
+<form action="searching.php">
 <select name="keywordsType">
-<option value="name" selected>name</option>
+<option value="name" <?php if($_GET['keywordsType']=="name")echo "selected"; ?>>name</option>
 <option value="description">description</option>
 <option value=""> </option>
 </select>
-<input type="text" name="keywordsInput" placeholder="please enter the keywords you want to search">
+<input type="text" name="keywordsInput" placeholder="please enter the keywords you want to search" value="<?php
+if(isset($_SESSION['keywords']))echo $_SESSION['keywords'];
+?>
+">
 <input type="submit" value="search">
 </form>
 <div id="result">
-<?php 
-if(isset($_POST['result'])){
-    echo "<div></div>";
-    echo $_POST['result'];
+<?php
+if(isset($_SESSION['resultSet'])){
+  $prossessResult=new mysqli_result();
+  $count=$prossessResult->num_rows/20+1;
+
 }
 ?>
+</div>
+<div name="pages">
+  <?php
+
+   ?>
 </div>
 </body>
 </html>
