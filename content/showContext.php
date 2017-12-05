cc063888b58ff35370b37693b7a84e0d838f2c32
c-guozhi90 <c.guozhi90@gmail.com>
2017-12-05 20:35:18 +0800

    Revert "Merge branch 'master' of https://github.com/KennyFung819/COMPS356-IT-DOG"
    This reverts commit b536db67af14bc52bd9182a3834c6da7edeb784d, reversing
    changes made to 8555587da3aba52475b1be1504c8489396324517.

--- content/showContext.php
+++ content/showContext.php
@@ -13,50 +13,111 @@ if ($mysql->connect_error){
 }
 
 
-class content{
-    private $subtitle =array();
-    private $content =array();
-    private $max;
+class kolData{
+    private $id;
+    private $type =array();
+    private $context1 =array();
+    private $context2 =array();
+    private $context3 =array();
+    private $context4 =array();
+    private $context5 =array();
+    private $context6 =array();
+    private $indexCount;
+    /*
+    //Setter & Getter for ID
+    public function getID(){
+        echo $this->id;
+    }
+    public function setID($id){
+        $this->id=$id;
+    }
+    */
 
-    //Setter & Getter for SubTitle
-    public function getSubTitle($sid){
-    echo $this->subtitle[$sid];
-}
-    public function setSubTitle($subtitle,$sid){
-        $this->subtitle[$sid]=$subtitle;
+    //Setter & Getter for Type
+    public function getType($id){
+        echo $this->type[$id];
     }
-    public function getContent($sid){
-        echo $this->content[$sid];
-    }
-    public function setContent($content,$sid){
-        $this->content[$sid]= $content;
-    }
-    public function getMax(){
-        return $this->max;
-    }
-    public function setMax($max){
-        $this->max= $max;
+    public function setType($type,$id){
+        $this->type[$id]=$type;
     }
 
+    //Setter & Getter for Context1
+    public function getContext1($id){
+        echo $this->context1[$id];
+    }
+    public function setContext1($context,$id){
+        $this->context1[$id]=$context;
+    }
 
+    //Setter & Getter for Context2
+    public function getContext2($id){
+        echo $this->context2[$id];
+    }
+    public function setContext2($context,$id){
+        $this->context2[$id]=$context;
+    }
+
+    //Setter & Getter for Context
+    public function getContext3($id){
+        echo $this->context3[$id];
+    }
+    public function setContext3($context,$id){
+        $this->context3[$id]=$context;
+    }
+
+    //Setter & Getter for Context
+    public function getContext4($id){
+        echo $this->context4[$id];
+    }
+    public function setContext4($context,$id){
+        $this->context4[$id]=$context;
+    }
+
+    //Setter & Getter for Context
+    public function getContext5($id){
+        echo $this->context5[$id];
+    }
+    public function setContext5($context,$id){
+        $this->context5[$id]=$context;
+    }
+
+    //Setter & Getter for Context6
+    public function getContext6($id){
+        echo $this->context6[$id];
+    }
+    public function setContext6($context,$id){
+        $this->context6[$id]= $context;
+    }
+
+    public function getCount(){
+        return $this->indexCount;
+    }
+    public function setCount($count){
+        $this->indexCount= $count;
+    }
 
     public function searchTarget($pid) {
 
+
+        $target_temp = "SELECT type,context1,context2,context3,context4,context5,context6 FROM kol_data WHERE pid='$pid'";
+
+        $counter=0;
         global $mysql;
-
-        $target_temp = "SELECT subtitle,content,load_order FROM wiki WHERE pid='$pid' ORDER BY load_order";
-
-        $max = $mysql->query($target_temp)->num_rows;
-
-        $this->setMax($max);
 
 
         if ($target= $mysql->query($target_temp)) {
             // output data of each rowz
             while($row = $target->fetch_assoc()) {
-                $this->setSubTitle($row["subtitle"],$row["load_order"]);
-                $this->setContent($row["content"],$row["load_order"]);
+                $this->setType($row["type"],$counter);
+                $this->setContext1($row["context1"],$counter);
+                $this->setContext2($row["context2"],$counter);
+                $this->setContext3($row["context3"],$counter);
+                $this->setContext4($row["context4"],$counter);
+                $this->setContext5($row["context5"],$counter);
+                $this->setContext6($row["context6"],$counter);
+                $counter++;
             }
+            $this->setCount($counter);
             $target->free();
         }
     }
--- searchingResult.php
+++ searchingResult.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,22 +9,55 @@
       <title>KOLpedia - search kol Here</title>
 
       <!-- Bootstrap core CSS -->
-      <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
       <!-- Custom fonts for this template -->
-      <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+      <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
       <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
       <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
       <!-- Plugin CSS -->
-      <link href="../vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+      <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
       <!-- Custom styles for this template -->
-      <link href="../css/creative.css" rel="stylesheet">
+      <link href="css/creative.css" rel="stylesheet">
 </head>
 <body class="bg-white">
-
-<?php include "../navbar/navbar.php" ?>
+  <?php session_start() ?>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">KOLpedia</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Category</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Random Page</a>
+          </li>
+          <li class="nav-item">
+            <form class="navbar-form navbar-search my-lg-0" action="searching.php">
+            <div class="input-group">
+            <div class="input-group-btn">
+            <select class="btn btn-primary" name="keywordsType">
+            <option class="btn" value="name" selected>Name</option>
+            <option class="btn" value="platform">Platform</option>
+            </select>
+          </div>
+            <input type="text" class="form-control" name="keywordsInput" value="">
+            <div class="input-group-btn">
+            <input type="submit" class="btn  btn-outline-success my-2 my-sm-0" value="search">
+            </div>
+          </div>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <section class="text-white" id="introduction">
       <div class="container">
           <div class="row">
