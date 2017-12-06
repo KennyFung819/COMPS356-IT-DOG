dee6eb94cce932dc69cf2c2697db3e2436aaf912
DESKTOP-H8C6MG8\chufu <chufung97819>
2017-12-06 11:49:31 +0800

    Redo previous change

---
+++ content/random_page.php
@@ -0,0 +1,34 @@
+<?php
+
+class randomPage{
+    private $random;
+function __construct(){
+    $servername='localhost';
+    $username = 'root';
+    $password = 'Kappa819';
+    $db ='project';
+
+    $mysql = new mysqli($servername, $username, $password, $db);
+
+    $target_temp = "SELECT id FROM kol";
+
+    $max = $mysql->query($target_temp)->num_rows;
+
+    if ($mysql->connect_error){
+        die("connection failed:" . $mysql->connect_error);
+    }
+
+    $local_random = strval(rand(1,$max));
+
+    $this->setRandom($local_random);
+
+}
+
+function setRandom($random){
+    $this->random =$random;
+}
+function getRandom(){
+    echo $this->random;
+}
+}
+?>
