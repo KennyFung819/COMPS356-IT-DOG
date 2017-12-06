38fa4b13a7964d21edb20dcb12578ae7b889bc81
DESKTOP-H8C6MG8\chufu <chufung97819>
2017-12-06 14:46:37 +0800

    fix bugs: comment section - show the comments and commenting form(hide it if user do not log in) searchResult.php - deleted the spaces in the input box

--- content/comments.php
+++ content/comments.php
@@ -1,10 +1,28 @@
 <!DOCTYPE HTML>
  <html>
  <div class="container">
+   <?php
+   $conn=connectDB();
+   if(!$_POST&&$conn&&isset($_GET['targetKol'])){
+     $commentText=displayComment($conn,(int)$_GET['targetKol']);
+     echo $commentText;
+     $conn->close();
+   }
+    elseif($_POST&&$conn&&isset($_GET['targetKol'])){
+      insertComment($conn);
+      $commentText=displayComment($conn,(int)$_GET['targetKol']);
+      echo $commentText;
+      $conn->close();
+    }
+
+    ?>
+    <?php
+      if(!isset($_SESSION['username'])): echo "please <a href='../login/login.php'>log in</a>";
+    else: ?>
      <form  method="post">
          <div class="form-group">
              <div class="form-group row">
-                 <label for="name" class="col-lg-2  col-form-label"> Name:</label><input type="text" readonly class="form-control-plaintext" id="name">
+                 <label for="name" class="col-lg-2  col-form-label">Your name: <?php echo $_SESSION['username'];?></label>
              </div>
              <div class="form-group row">
                 <label for="comment" class="col-lg-12  col-form-label"> Comments: </label>
@@ -18,63 +36,43 @@
                 <button type="submit" class="btn btn-primary col-lg-2 col-md-4 col-sm-8" name="submit-comment">Submit</button>
             </div>
          </div>
-     </form>
+     </form>}
+   <?php endif?>
+
  </div>
  <!-- Bootstrap core JavaScript -->
  <script src="../vendor/jquery/jquery.min.js"></script>
  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
  </html>
- 
+
  <?php
-<<<<<<< HEAD
- function insertComment()
+ function insertComment(mysqli $mysql)
  {
-     if (isset($_POST['userComment'])||empty($_POST['userComment'])) {
+     if (!isset($_POST['userComment'])||empty($_POST['userComment'])) {
          #POP A ALERT IF USER DO NOT INPUT ANY THING
-         alert("the comment cannot be empty");
-         die;
-     } elseif (!isset($_SESSION['username'])||isset(empty($_SESSION['username']))) {
-       #pop an alert if user do not login
-         alert("please login");
-         die;
-     } elseif ($_POST) {
+         alert("The comment should not be empty");
+     } elseif (!isset($_SESSION['username'])||empty($_SESSION['username'])) {
+        #pop an alert if user do not login
+          alert("please login");
+        }else {
          #GET THE FORM DATA
-  $name = $_SESSION['username'];#user name from session
-  $pid=$_GET['targetKol'];
-         $message = str_replace("'", "&apos", trim($_POST["userComment"]));#replace single quotes
-         $time=time();
-
-         #Connet to database
-         $servername='localhost';
-         $username = 'root';
-<<<<<<< HEAD
-         $password = '';
-=======
-         $password = '';
->>>>>>> master-commentSection-Raven
-         $db ='project';
-         $mysql = new mysqli($servername, $username, $password, $db);
-         if ($mysql->connect_error) {
-             die("connection failed:" . $mysql->connect_error);
-         }
-         $insert = "INSERT INTO comment(pid,uname,comment_text,timeofcomment) VALUES($pid,$_SESSION['username'], $message,$time)";
-
+         $name = $_SESSION['username'];#user name from session
+         $pid=$_GET['targetKol'];
+         $comment = str_replace("'", "&apos", trim($_POST["userComment"]));#replace single quotes
+         $uname_temp=$_SESSION['username'];
+         $insert="INSERT INTO comment(pid,uname,comment_text,timeofcomment) VALUES($pid,$uname_temp,$comment,now())";
          #WRITE DOWN COMMENTS#
          if ($mysql->query($insert)) {
              alert("comment succeeded!");
          } else {
              alert("comment failed!");
          }
-         $mysql->close();
      }
  }
 
 /**
 * display the top 5 comments most currently, it will return strings with html tags
-<<<<<<< HEAD
-=======
 * please remenber to close the database connection after invoking this function
->>>>>>> master-commentSection-Raven
 *
 * @param mysqli $mysql the database connection
 * @param int $pid the id of kol
@@ -83,90 +81,43 @@
 function displayComment(mysqli $mysql, int $pid)
 {
     $resultWithHtml="";
-<<<<<<< HEAD
-    $query="SELECT PID,UID,COMMENT_TEXT,TIMEOFCOMMENT FROM COMMENT WHERE PID=$pid LIMITI 5 ORDER BY TIMEOFCOMMENT DESC";
-    $stmt=$mysql->prepare($query);
-    $stmt->bind_result($pid, $uid, $comment_text, $timeofcomment);
-    $stmt->execute();
-    $stmt->store_result();
-=======
-    $query="SELECT PID,UNAME,COMMENT_TEXT,TIMEOFCOMMENT FROM COMMENT WHERE PID=$pid LIMITI 5 ORDER BY TIMEOFCOMMENT DESC";
-    $stmt=$mysql->prepare($query);
-    $stmt->bind_result($pid, $uname, $comment_text, $timeofcomment);
-    $stmt->execute();
-    $stmt->store_result();
-
->>>>>>> master-commentSection-Raven
-    if ($stmt->num_rows==0) {
-        $resultWithHtml="There is no comment here. Be the first one!";
-    } else {
-        while ($stmt->fetch()) {
-<<<<<<< HEAD
-            $resultWithHtml=$resultWithHtml."
-          <div class='container'>
-          <div class='col-md-4 col-lg-4'><h4>$uid</h4></div>
-          <divclass='col-md-8 col-lg-8'><p>$comment_text</p><hr><p>$timeofcomment</p></div>
-=======
-          $timetoprint=new date('H:i M d, Y',$timeofcomment);
-          $resultWithHtml=$resultWithHtml."
-          <div class='container'>
-          <div class='col-md-4 col-lg-4'><h4>$uname</h4></div>
-          <div class='col-md-8 col-lg-8'><p>$comment_text</p><hr><p>$timetoprint</p></div>
->>>>>>> master-commentSection-Raven
-          </div>
-          ";
-        }
+    $pid;$uname;$comment_text;$timeofcomment;
+    $query="SELECT PID,UNAME,COMMENT_TEXT,TIMEOFCOMMENT FROM COMMENT WHERE PID=$pid  ORDER BY TIMEOFCOMMENT DESC LIMIT 5";
+    if($stmt=$mysql->prepare($query)){
+      $stmt->execute();
+      $stmt->bind_result($pid, $uname, $comment_text, $timeofcomment);
+      $stmt->store_result();
+      while ($stmt->fetch()) {
+        $timeofcomment=strtotime($timeofcomment);
+        $timetoprint=date('H:i  M d, Y',$timeofcomment);
+        $resultWithHtml=$resultWithHtml."
+        <div class='container'>
+        <div class='col-md-4 col-lg-4'><h4>$uname</h4></div>
+        <div class='col-md-8 col-lg-8'><p>$comment_text</p><hr><p>$timetoprint</p></div>
+        </div>";
+      }
+    }else{
+      $resultWithHtml="<p>There is no comment here. Be the first one!</p>";
+    }
         return $resultWithHtml;
     }
+
+function connectDB(){
+  #Connet to database
+  $servername='localhost';
+  $username = 'root';
+  $password = '';
+  $db ='project';
+  $mysql = new mysqli($servername, $username, $password, $db);
+  if ($mysql->connect_error) {
+      alert("connection failed:" . $mysql->connect_error);
+      return null;
+  }
+  return $mysql;
 }
 
  function alert($msg)
  {
-=======
-
-
-
- if (isset($_POST['userComment'])) {
-     #POP A ALERT IF USER DO NOT INPUT ANY THING
-     alert("The comment cannot be empty");
-     die;
- }
-
- #Connet to database
-
- $servername='localhost';
- $username = 'root';
- $password = 'Kappa819';
- $db ='project';
-
- $name = $_POST["userName"];
- $message = $_POST["userComment"];
-
- $mysql = new mysqli($servername, $username, $password, $db);
- if ($mysql->connect_error){
-     die("connection failed:" . $mysql->connect_error);
- }
-
- $pid=$_GET['targetKol'];
-
- if($_POST){
-
-
-
- $insert = "INSERT INTO comment(pid,uid,comment_text) VALUES($pid,uid, $message) FROM comment WHERE pid='$pid' ORDER BY id";
-
- #GET THE FORM DATA
-
-
-
- }
- #WRITE DOWN COMMENTS#
-
-#DISPLAY COMMENTS#
-
-
- function alert($msg) {
->>>>>>> Revert "display comments"
      echo "<script type='text/javascript'>alert('$msg');</script>";
  }
 ?>
